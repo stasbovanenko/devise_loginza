08ef7569f04a4e26a42d9774b4a4cb7560571b4a
Sasha <gotacid@gmail.com>
2012-04-14 19:46:50 +0400

    fix loginza routes

--- lib/devise_loginza/routes.rb
+++ lib/devise_loginza/routes.rb
@@ -3,7 +3,7 @@ ActionDispatch::Routing::Mapper.class_eval do
   protected
 
   def devise_loginza(mapping, controllers) #:nodoc:
-    resource :session, :only => [], :controller => controllers[:loginza_sessions], :path => "" do
+    resource :session, :controller => controllers[:loginza_sessions], :path => "" do
       match :loginza, :path => mapping.path_names[:loginza]
     end
 
